48e277f9bbea2ce25f510b1d5ccc3981e2725086
chameleon001 <chameleon001@naver.com>
2017-08-16 12:48:01 +0900

    세명대학교 추가
    세명대학교 추가 (약자가 smu인데 선문대하고 겹쳐 semyung로 썼습니다)

--- univColor.js
+++ univColor.js
@@ -92,6 +92,7 @@ function getColorByUnivName(univ) {
   if (univ == "SMU" || univ.includes("선문대")) return "#006a74";
   if (univ == "SKHU" || univ.includes("성공회대")) return "#006BB1";
   if (univ == "SKKU" || univ.includes("성균관대")) return "#8DC63F";
+  if (univ == "SEMYUNG" || univ.includes("세명대")) return "#005DAA";
   if (univ == "SJU" || univ.includes("세종대")) return "#ba0c2f";
   if (univ == "SSU" || univ.includes("숭실대")) return "#4badcd";
   if (univ == "SWC" || univ.includes("숭의여대")) return "#062d8f";
