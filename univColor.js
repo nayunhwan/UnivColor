78440e1dbb09e3c504dab20fc5e7b95ea1a93e9f
Kwoneunhye <31054584+Kwoneunhye@users.noreply.github.com>
2017-08-16 18:08:45 +0900

    Update univColor.js
    color correction

--- univColor.js
+++ univColor.js
@@ -111,7 +111,7 @@ function getColorByUnivName(univ) {
   if (univ == "AJU" || univ.includes("아주대")) return "#0072ce";
   if (univ == "YSU" || univ.includes("연세대")) return "#16407d";
   if (univ == "YU" || univ.includes("영남대")) return "#003e7d";
-  if (univ == "INU" || univ.includes("인천대")) return "#0D3BE6";
+  if (univ == "INU" || univ.includes("인천대")) return "#0030f2";
   if (univ == "INHA" || univ.includes("인하대")) return "#0267b9";
 
   if (univ == "JNU" || univ.includes("전남대")) return "#00913F";
